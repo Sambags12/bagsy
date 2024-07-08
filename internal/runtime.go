34f17463defdea9d9528b514692cbad3f431ae06
ExoMonk <0xexomonk@gmail.com>
2024-07-08 10:22:00 +0100

    feat: market resolver

--- internal/runtime.go
+++ internal/runtime.go
@@ -89,3 +89,4 @@
 // 2024-07-07T20:43:00 fix: agent risk parameter
 // 2024-07-07T17:04:00 fix: oracle feed handler
 // 2024-07-08T10:17:00 feat: market resolver
+// 2024-07-08T10:22:00 feat: market resolver
