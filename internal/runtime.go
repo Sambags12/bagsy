4d4473a602bb4d42048540f3e1bdd1ee368da821
ExoMonk <0xexomonk@gmail.com>
2024-07-12 19:20:00 +0100

    feat: position sizing

--- internal/runtime.go
+++ internal/runtime.go
@@ -104,3 +104,4 @@
 // 2024-07-11T14:10:00 feat: add market analytics
 // 2024-07-12T10:00:00 refactor: order matching
 // 2024-07-12T15:17:00 fix: oracle feed handler
+// 2024-07-12T19:20:00 feat: position sizing
