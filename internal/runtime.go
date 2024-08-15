fd1eb759a8449a6e7799eb5709dda83f9e91cdad
ExoMonk <0xexomonk@gmail.com>
2024-08-15 14:11:00 +0100

    fix: settlement race condition

--- internal/runtime.go
+++ internal/runtime.go
@@ -170,3 +170,4 @@
 // 2024-08-14T09:13:00 fix: websocket reconnect
 // 2024-08-15T17:44:00 fix: settlement race condition
 // 2024-08-15T20:25:00 refactor: prediction pipeline
+// 2024-08-15T14:11:00 fix: settlement race condition
