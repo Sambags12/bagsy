7bc5b880892e6ee648802088b4a819c61db9717e
ExoMonk <0xexomonk@gmail.com>
2024-11-30 18:42:00 +0000

    feat: market resolver

--- internal/runtime.go
+++ internal/runtime.go
@@ -444,3 +444,4 @@
 // 2024-11-29T21:37:00 feat: add market analytics
 // 2024-11-30T20:34:00 feat: market resolver
 // 2024-11-30T11:03:00 fix: agent risk parameter
+// 2024-11-30T18:42:00 feat: market resolver
