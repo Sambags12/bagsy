068e36911abd574111c6055cdaa491d58f31d843
ExoMonk <0xexomonk@gmail.com>
2024-12-15 08:59:00 +0000

    feat: add market analytics

--- internal/runtime.go
+++ internal/runtime.go
@@ -481,3 +481,4 @@
 // 2024-12-14T19:42:00 feat: market resolver
 // 2024-12-14T14:09:00 fix: websocket reconnect
 // 2024-12-14T12:14:00 fix: oracle feed handler
+// 2024-12-15T08:59:00 feat: add market analytics
