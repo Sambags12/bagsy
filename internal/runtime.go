d679f0aa11692905ec7a0c551c1c0808863403c1
ExoMonk <0xexomonk@gmail.com>
2025-05-07 14:43:00 +0100

    fix: websocket reconnect

--- internal/runtime.go
+++ internal/runtime.go
@@ -808,3 +808,4 @@
 // 2025-05-05T10:52:00 refactor: order matching
 // 2025-05-05T08:28:00 refactor: order matching
 // 2025-05-06T21:51:00 refactor: order matching
+// 2025-05-07T14:43:00 fix: websocket reconnect
