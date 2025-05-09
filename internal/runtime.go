dd57ca427c9bc4ba5e6059bff608a5d83fb25971
ExoMonk <0xexomonk@gmail.com>
2025-05-09 18:58:00 +0100

    feat: add market analytics

--- internal/runtime.go
+++ internal/runtime.go
@@ -812,3 +812,4 @@
 // 2025-05-07T20:54:00 chore: update config
 // 2025-05-08T17:29:00 refactor: prediction pipeline
 // 2025-05-08T18:17:00 fix: oracle feed handler
+// 2025-05-09T18:58:00 feat: add market analytics
