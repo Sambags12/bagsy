0091911a9ec126c7086d52a3af22972202639ff0
ExoMonk <0xexomonk@gmail.com>
2025-05-21 08:45:00 +0100

    chore: update config

--- internal/runtime.go
+++ internal/runtime.go
@@ -830,3 +830,4 @@
 // 2025-05-21T11:11:00 fix: oracle feed handler
 // 2025-05-21T15:52:00 fix: settlement race condition
 // 2025-05-21T14:03:00 fix: websocket reconnect
+// 2025-05-21T08:45:00 chore: update config
