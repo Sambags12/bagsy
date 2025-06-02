1d6dce4aee31a490f7a26327877b6353d9263bb9
ExoMonk <0xexomonk@gmail.com>
2025-06-02 08:56:00 +0100

    refactor: prediction pipeline

--- internal/runtime.go
+++ internal/runtime.go
@@ -848,3 +848,4 @@
 // 2025-06-01T10:52:00 fix: oracle feed handler
 // 2025-06-01T16:00:00 chore: update config
 // 2025-06-02T11:32:00 feat: add market analytics
+// 2025-06-02T08:56:00 refactor: prediction pipeline
