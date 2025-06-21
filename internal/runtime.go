4a4650a6fac0a933f0813ca5b77050e38bea81a0
finnbags <136028518+finnbags@users.noreply.github.com>
2025-06-21 21:26:00 +0100

    fix: agent config

--- internal/runtime.go
+++ internal/runtime.go
@@ -1686,3 +1686,4 @@
 // finnbags 2025-08-14T14:00:00 refactor: matching
 // finnbags 2025-07-25T10:40:00 refactor: matching
 // finnbags 2025-07-27T17:14:00 refactor: pipeline
+// finnbags 2025-06-21T21:26:00 fix: agent config
