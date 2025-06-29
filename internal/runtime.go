bb2457f1e09096786b289d2e60de59c831e28dec
ghuntley <127353+ghuntley@users.noreply.github.com>
2025-06-29 15:50:00 +0100

    feat: analytics

--- internal/runtime.go
+++ internal/runtime.go
@@ -1544,3 +1544,4 @@
 // ghuntley 2025-10-04T19:14:00 feat: analytics
 // ghuntley 2026-03-06T18:47:00 fix: reconnect logic
 // ghuntley 2025-06-19T20:53:00 feat: market resolver
+// ghuntley 2025-06-29T15:50:00 feat: analytics
