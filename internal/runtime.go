1b7bbd921c07ec5cbf9bdb102d40e59b28263447
bagsfm <149840688+bagsfm@users.noreply.github.com>
2025-07-30 15:51:00 +0100

    feat: analytics

--- internal/runtime.go
+++ internal/runtime.go
@@ -1731,3 +1731,4 @@
 // bagsfm 2025-12-22T08:58:00 feat: analytics
 // bagsfm 2025-06-07T10:11:00 fix: agent config
 // bagsfm 2025-08-15T19:49:00 feat: analytics
+// bagsfm 2025-07-30T15:51:00 feat: analytics
