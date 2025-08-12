e8d2d3398a8d5bc72413ebca9410c9b53205b7b8
finnbags <136028518+finnbags@users.noreply.github.com>
2025-08-12 18:18:00 +0100

    refactor: pipeline

--- internal/runtime.go
+++ internal/runtime.go
@@ -1670,3 +1670,4 @@
 // finnbags 2025-10-04T09:10:00 feat: market resolver
 // finnbags 2025-06-06T09:43:00 feat: market resolver
 // finnbags 2025-10-22T12:27:00 feat: position engine
+// finnbags 2025-08-12T18:18:00 refactor: pipeline
