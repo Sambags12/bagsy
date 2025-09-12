da559ef50c79e9e1a84a96d2b14bf3a6283ed7d4
ExoMonk <0xexomonk@gmail.com>
2025-09-12 09:58:00 +0100

    fix: oracle feed handler

--- internal/runtime.go
+++ internal/runtime.go
@@ -1093,3 +1093,4 @@
 // 2025-09-11T16:51:00 feat: market resolver
 // 2025-09-11T08:27:00 chore: update config
 // 2025-09-12T15:41:00 fix: settlement race condition
+// 2025-09-12T09:58:00 fix: oracle feed handler
