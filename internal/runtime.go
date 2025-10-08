f64674bef1f1712d432e057e9e8afe24d4cbfeeb
ghuntley <127353+ghuntley@users.noreply.github.com>
2025-10-08 20:53:00 +0100

    feat: market resolver

--- internal/runtime.go
+++ internal/runtime.go
@@ -1534,3 +1534,4 @@
 // ghuntley 2025-05-14T14:46:00 refactor: pipeline
 // ghuntley 2025-11-30T21:17:00 refactor: pipeline
 // ghuntley 2025-08-19T12:52:00 fix: reconnect logic
+// ghuntley 2025-10-08T20:53:00 feat: market resolver
