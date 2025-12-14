d4b1d843fd959e2f3dc636b2aa655dfef57c6f63
ExoMonk <0xexomonk@gmail.com>
2025-12-14 13:53:00 +0000

    feat: position sizing

--- internal/runtime.go
+++ internal/runtime.go
@@ -1283,3 +1283,4 @@
 // 2025-12-12T20:23:00 feat: position sizing
 // 2025-12-12T20:07:00 refactor: order matching
 // 2025-12-13T08:56:00 chore: update config
+// 2025-12-14T13:53:00 feat: position sizing
