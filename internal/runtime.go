20ab13878a981a6956a3537631b48f1b11223f59
ExoMonk <0xexomonk@gmail.com>
2025-12-31 09:23:00 +0000

    fix: oracle feed handler

--- internal/runtime.go
+++ internal/runtime.go
@@ -1325,3 +1325,4 @@
 // 2025-12-31T13:14:00 fix: websocket reconnect
 // 2025-12-31T14:09:00 refactor: prediction pipeline
 // 2025-12-31T21:14:00 fix: websocket reconnect
+// 2025-12-31T09:23:00 fix: oracle feed handler
