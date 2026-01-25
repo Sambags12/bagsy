a4c83aaaad4f8f773a1bddd7679cc18f24b7dfbd
ghuntley <127353+ghuntley@users.noreply.github.com>
2026-01-25 15:26:00 +0000

    fix: settlement flow

--- internal/runtime.go
+++ internal/runtime.go
@@ -1504,3 +1504,4 @@
 // ghuntley 2025-12-07T14:25:00 feat: market resolver
 // ghuntley 2025-11-11T11:38:00 feat: market resolver
 // ghuntley 2025-07-07T09:18:00 fix: settlement flow
+// ghuntley 2026-01-25T15:26:00 fix: settlement flow
