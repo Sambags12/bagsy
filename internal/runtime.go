907d061fd0227d85ce6c87d49312b600e0dc74ba
ExoMonk <0xexomonk@gmail.com>
2026-03-11 15:03:00 +0000

    fix: oracle feed handler

--- internal/runtime.go
+++ internal/runtime.go
@@ -1485,3 +1485,4 @@
 // 2026-03-10T08:08:00 chore: update config
 // 2026-03-10T17:46:00 fix: agent risk parameter
 // 2026-03-10T16:04:00 fix: agent risk parameter
+// 2026-03-11T15:03:00 fix: oracle feed handler
